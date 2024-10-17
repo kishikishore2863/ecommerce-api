ecaf949fdef6243bcdd9619193c47765ad3be524
kishikishore2863 <kishikishore.2863@gmail.com>
2024-10-17 23:58:31 +0530

    added products controller

---
+++ src/main/java/com/kishi/ecommerce_api/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.kishi.ecommerce_api.controller;
+
+
+import com.kishi.ecommerce_api.exception.ProductException;
+import com.kishi.ecommerce_api.model.Product;
+import com.kishi.ecommerce_api.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    private ProductService productService;
+
+
+    @PostMapping("/products")
+    public ResponseEntity<Page<Product>> findProductByCategoryHandler(@RequestParam String category,
+                                                                      @RequestParam List<String>color,
+                                                                      @RequestParam List<String> size,
+                                                                      @RequestParam Integer minPrice,
+                                                                      @RequestParam Integer maxPrice,
+                                                                      @RequestParam Integer minDiscount,
+                                                                      @RequestParam String sort,
+                                                                      @RequestParam String stock,
+                                                                      @RequestParam Integer pageNumber,
+                                                                      @RequestParam Integer pageSize
+                                                                      ){
+        Page<Product>res=productService.getAllProduct(category,color,size,minPrice,maxPrice,minDiscount,sort,stock,pageNumber,pageSize);
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/product/{productId}")
+    public ResponseEntity<Product>findProductByIdHandler(@PathVariable Long productId)throws ProductException{
+        Product product = productService.findProductById(productId);
+        return new ResponseEntity<Product>(product,HttpStatus.ACCEPTED);
+    }
+
+
+
+}
